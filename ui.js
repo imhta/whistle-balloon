4f121b71a24be792ec51cb1103e820a0d8d28c94
imhtop <nponmuthuselvam@gmail.com>
2019-07-22 10:31:20 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,31 +1,35 @@
-class CreateUi{
-    constructor() {
-        this.stateOfbutton = false;
-        this.createElements();
-        this.applyPosition();
-        this.applyStyle();
-        this.applyFunctionality();
-    }
+class CreateUi {
+  constructor() {
+    this.stateOfbutton = false;
+    this.createElements();
+    this.applyPosition();
+    this.applyStyle();
+    this.applyFunctionality();
+  }
 
-    createElements() {
-        this.pauseButton = createButton('Pause');
+  createElements() {
+    this.startButton = createButton("Start / Pause");
+    this.levelState = createElement('p', '');
+  }
+  applyPosition() {
+    this.startButton.position(19, 19);
+    this.levelState.position(170, 12);
+  }
+  updateLevelState(text){
+    this.levelState.html(text);
+  }
+  applyStyle() {}
+  applyFunctionality() {
+    this.startButton.mousePressed(this.changeButton);
+  }
+  changeButton() {
+    if (!this.stateOfbutton) {
+      this.stateOfbutton = true;
+      this.startButton = createButton("Pause");
+      loop();
+    } else {
+      this.stateOfbutton = false;
+      noLoop();
     }
-    applyPosition() {
-        this.pauseButton.position(19, 19);
-    }
-    applyStyle() {
-    }
-    applyFunctionality() {
-        this.pauseButton.mousePressed(this.changeButton);
-    }
-    changeButton() {
-        if (!this.stateOfbutton) {
-            this.stateOfbutton = true;
-            noLoop();
-        } else {
-            this.stateOfbutton = false;
-            loop();
-        }
-    }
+  }
 }
-
